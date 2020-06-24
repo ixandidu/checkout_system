e574178bee1654621234b76aaa729f9feae8c34a
Ikhsan Maulana <ixandidu@gmail.com>
2020-06-25 01:29:51 +0700

    Core logic are all in `Promotion` now

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,31 +4,17 @@ require 'promotion'
 class Checkout
   def initialize(promotions)
     @scanned_items = []
-
-    @item_promotions = promotions.filter do |promotion|
-      promotion.is_a?(Promotion::Item)
-    end
-
-    @basket_promotions = promotions.filter do |promotion|
-      promotion.is_a?(Promotion::Basket)
-    end
+    @promotions = promotions
   end
 
   def scan(item)
-    if (scanned_item = @scanned_items.find { |i| i.item == item })
-      scanned_item.rescan
-    else
-      @scanned_items << ScannedItem.new(item)
-    end
+    scanned_item = @scanned_items.find { |i| i.item == item }
+    return scanned_item.rescan && @scanned_items if scanned_item
+
+    @scanned_items << ScannedItem.new(item)
   end
 
   def total
-    @item_promotions.each do |item_promotion|
-      item_promotion.apply(@scanned_items)
-    end
-
-    @basket_promotions.sum do |basket_promotion|
-      basket_promotion.apply(@scanned_items.sum(&:billable_amount))
-    end
+    Promotion.apply(@promotions, @scanned_items)
   end
 end
